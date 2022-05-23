9221659fd6e8db9f1746e554f6e5ef269a2d3e90
vibhorchhabra321 <48565719+vibhorchhabra321@users.noreply.github.com>
2022-05-23 20:16:27 +0530

    UTX-2

---
+++ jira_status_checker.py
@@ -0,0 +1,27 @@
+from operator import truediv
+from jira import JIRA
+import os
+import warnings
+import sys
+
+
+jira_id = sys.argv[1]
+jira_base_url = os.environ.get("JIRA_CLOUD_URL")
+username = os.environ.get("JIRA_USER")
+pwd = os.environ.get("JIRA_TOKEN")
+
+jira = JIRA(options = jira_base_url,
+			basic_auth = (username,
+						pwd))
+
+singleIssue = jira.issue(jira_id)
+print('{}: {}:{}'.format(singleIssue.key,
+						singleIssue.fields.summary,
+                        singleIssue.fields.status,
+						singleIssue.fields.reporter.displayName))
+print(singleIssue)
+if singleIssue exist return truedi
+if singleIssue.status in ["To Do", "Done"]:
+    return False
+elif singleIssue.status in ["In Progress"]:
+    return true
