cba9c4dde1ab717acbfe04df3ed82e2d1e32e287
vibhorchhabra321 <48565719+vibhorchhabra321@users.noreply.github.com>
2022-05-25 17:39:35 +0530

    UTX-1

--- jira_status_checker.py
+++ jira_status_checker.py
@@ -7,8 +7,8 @@ import sys
 
 jira_id = sys.argv[1]
 jira_base_url = os.environ.get("JIRA_CLOUD_URL")
-username = os.environ.get("JIRA_USER")
-apikey = os.environ.get("JIRA_TOKEN")
+username = os.environ.get("JIRA_USER_LOCAL")
+apikey = os.environ.get("JIRA_TOKEN_LOCAL")
 
 jira = JIRA(options = {'server':jira_base_url, 'verify':False}, basic_auth = (username, apikey))
 
