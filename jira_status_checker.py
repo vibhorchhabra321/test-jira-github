81ebff1d969439a884d586913926b882673d8bdf
vibhorchhabra321 <48565719+vibhorchhabra321@users.noreply.github.com>
2022-05-25 17:23:40 +0530

    UTX-1

--- jira_status_checker.py
+++ jira_status_checker.py
@@ -10,15 +10,10 @@ jira_base_url = os.environ.get("JIRA_CLOUD_URL")
 username = os.environ.get("JIRA_USER")
 pwd = os.environ.get("JIRA_TOKEN")
 
-jira = JIRA(options = jira_base_url,
-			basic_auth = (username,
-						pwd))
+jira = JIRA(options = jira_base_url, basic_auth = (username, pwd))
 
 singleIssue = jira.issue(jira_id)
-print('{}: {}:{}'.format(singleIssue.key,
-						singleIssue.fields.summary,
-                        singleIssue.fields.status,
-						singleIssue.fields.reporter.displayName))
+print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary, singleIssue.fields.status, singleIssue.fields.reporter.displayName))
 print(singleIssue)
 
 def jira_status_checker():
