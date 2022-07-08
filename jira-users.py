c026de4d272af87ed65ce12ed1eb62db7688c07a
Vibhor Chhabra <48565719+vibhorchhabra321@users.noreply.github.com>
2022-07-08 14:07:43 +0530

    UTX-1

--- jira-users.py
+++ jira-users.py
@@ -5,7 +5,7 @@ import warnings
 import sys
 
 
-jira_id = sys.argv[1]
+#jira_id = sys.argv[1]
 jira_base_url = os.environ.get("JIRA_CLOUD_URL")
 username = os.environ.get("JIRA_USER_LOCAL")
 apikey = os.environ.get("JIRA_TOKEN_LOCAL")
